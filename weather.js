d6cc66a19337f1c20c5e4131b241e1b88792e785
ReshmaRajK <reshmarajmsm@gmail.com>
2023-07-01 17:21:55 +0530

    description added

--- weather.js
+++ weather.js
@@ -1,34 +1,35 @@
-function setData(){
-    names=cityName.value
+function setData() {
+    names = cityName.value
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${names}&appid=5fe36b192ffd1c36dffb6752bc1722b2`)
-    .then(data => data.json()).then(data => displayWeather(data))
+        .then(data => data.json()).then(data => displayWeather(data))
     function displayWeather(Cdata) {
         country = Cdata.sys.country
         humidity = Cdata.main.humidity
         wind = Cdata.wind.speed
         pressure = Cdata.main.pressure
         temp = eval(Cdata.main.temp - 273.15).toFixed()
-        feel = eval(Cdata.main.feels_like-273.15).toFixed()
+        feel = eval(Cdata.main.feels_like - 273.15).toFixed()
         city = Cdata.name
+        description = Cdata.weather[0].description
 
-        country1.innerHTML=`
+        country1.innerHTML = `
         <label class="text-white">Country</label>
         <p class="text-black">${country}</p>
         `
-        humidity1.innerHTML=`
+        humidity1.innerHTML = `
         <label class="text-white">Humidity</label>
         <p class="text-black">${humidity}%</p>
         `
-        wind1.innerHTML=`
+        wind1.innerHTML = `
         <label class="text-white">Wind</label>
         <p class="text-black">${wind} kmph</p>
         `
-        pressure1.innerHTML=`
+        pressure1.innerHTML = `
         <label class="text-white">Pressure</label>
         <p class="text-black">${pressure}</p>
         `
         temp1.innerHTML = `
-            <label style="font-size:25px;font-style: bold; font-weight: 500;" >${temp}℃</label>
+            <label style="font-size:25px;font-style: bold; font-weight: 500;" >${temp}℃,<span style="font-size:11px">${description}</span></label>
             <p style="font-size:12px" >feels like ${feel}℃</p>
             `
         cName.innerHTML = `
